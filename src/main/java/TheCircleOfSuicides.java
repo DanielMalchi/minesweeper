b6054a09713775701b0f6eccfcdf781b3b99bbdf
Daniel Malchi <96833272+DanielMalchi@users.noreply.github.com>
2022-01-12 09:52:11 +0200

    Update TheCircleOfSuicides.java

--- src/main/java/TheCircleOfSuicides.java
+++ src/main/java/TheCircleOfSuicides.java
@@ -62,6 +62,7 @@ public class TheCircleOfSuicides {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println(array());
+        //123
     }
 
 }
